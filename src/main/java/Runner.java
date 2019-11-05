b629bb0779b27750d07118389e6d20360de70bca
Kamil P <antyplastik@gmail.com>
2019-11-05 09:36:57 +0100

    insignificant changes in the runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,4 @@
+import domain.TestCard;
 import object_creation.ReadTestCardFromFile;
 import object_creation.param.status_and_exceptions.RecognizeParamTypeException;
 import object_creation.test_card.config.TestCardColumnsNumbers;
@@ -8,13 +9,15 @@ public class Runner {
     public static void main(String[] args) {
         String path = "D:\\KP\\Workspace\\Java\\testCard\\src\\test\\resources\\DED7850 Karta badań - Młotowiertarka_ref.csv";
 
+        TestCard testCard;
+
         TestCardColumnsNumbers testCardColumnsNumbers = new TestCardColumnsNumbers(0,1,2,2,3,4);
         TestCardParamMarks testCardParamMarks = new TestCardParamMarks("#","h","b","n","f","i");
         TestCardConfig testCardConfig = new TestCardConfig(testCardColumnsNumbers, testCardParamMarks);
 
         ReadTestCardFromFile testCardReader = new ReadTestCardFromFile(testCardConfig);
         try {
-            testCardReader.createTestCardFromFile(path);
+            testCard = testCardReader.createTestCardFromFile(path);
         } catch (RecognizeParamTypeException e) {
             System.out.println(e.getMessage());
         }
